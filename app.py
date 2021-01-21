67cae71667becb97384feca49c1e821c2e9b397d
giacomounivr <giacomo.marchioro@univr.it>
2021-01-21 10:02:13 +0100

    set cache age to 0 for custum.css dev.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ cnx = create_engine(databaseaddress_capitolare)
 df = pd.read_sql('SELECT * FROM descrizione_esterna', cnx) #read the entire table
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def index_page():
