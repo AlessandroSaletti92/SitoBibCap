60616b0b7a127369bedb3cfd1d0461aefa115bae
giacomounivr <giacomo.marchioro@univr.it>
2021-01-22 12:45:11 +0100

    Nuova versione dei risultati

--- config_db.py
+++ config_db.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
+from confidenziale import databaseaddress_capitolare
 
-cnx = create_engine('mysql+pymysql://root:@localhost/capitolare')    
+cnx = create_engine(databaseaddress_capitolare)
 df = pd.read_sql('SELECT * FROM descrizione_esterna', cnx) #read the entire table
-
-descrizione_esterna = pd.read_sql('Select * from descrizione_esterna where segnatura='$search';')
