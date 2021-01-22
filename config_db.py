2a731d2563ae1d3c0287e23be29a46d36b5700f1
giacomounivr <giacomo.marchioro@univr.it>
2021-01-22 14:11:38 +0100

    Configuraizone databse Sqlalchemy mapping

--- config_db.py
+++ config_db.py
@@ -2,6 +2,29 @@ import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
 from confidenziale import databaseaddress_capitolare
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
-cnx = create_engine(databaseaddress_capitolare)
-df = pd.read_sql('SELECT * FROM descrizione_esterna', cnx) #read the entire table
+Base = automap_base()
+
+# engine, suppose it has two tables 'user' and 'address' set up
+engine = create_engine(databaseaddress_capitolare)
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# mapped classes are now created with names by default
+# matching that of the table name.
+#User = Base.classes.user
+#Address = Base.classes.address
+
+session = Session(engine)
+
+
+# collection-based relationships are by default named
+# "<classname>_collection"
+print (u1.address_collection)
+
+
+df = pd.read_sql('SELECT * FROM descrizione_esterna', engine) #read the entire table
