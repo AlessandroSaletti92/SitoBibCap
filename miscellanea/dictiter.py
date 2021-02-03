81097368301d047620e0dda73e77f3da29e23a35
giacomounivr <giacomo.marchioro@univr.it>
2021-02-03 17:21:37 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ miscellanea/dictiter.py
@@ -0,0 +1,15 @@
+def get_all_values(nested_dictionary):
+    for key, value in nested_dictionary.items():
+        if isinstance(value,dict):
+            get_all_values(value)
+        if isinstance(value,list):
+            for i in value:
+                get_all_values(i)
+        else:
+            if (value == "") or (value is None):
+                nested_dictionary[key] = "Non disponibile"
+
+def replaceindict(dict):
+    dict['_id'] = 'n.a.'
+    res = str(dict).replace("''","'Non disponibile'").replace('None',"'Non disponibile'")
+    return eval(res)
